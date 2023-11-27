84475059f9eb18ab384b93d1e2f3b9feaaa48381
Armine Azizyan <armineazizyann@gmail.com>
2023-11-27 20:12:09 +0400

    Commiting test project

---
+++ cypress/e2e/test_login.cy.ts
@@ -0,0 +1,98 @@
+describe("test login",()=>{
+   beforeEach(()=>{
+   
+    cy.visit("https://www.saucedemo.com/v1/index.html"); 
+   
+   })
+ 
+
+
+it("successful login", ()=>{
+
+   cy.fixture('user_data.json').then((userData) =>{
+      userData.users.forEach((user)=>{
+       if(user.username == "standard_user"){
+	 cy.LogIn(user.username, user.password)	
+         cy.get("#login-button").click();
+	 cy.url().should("contain","/v1/inventory.html");
+         }
+      
+      })
+    }) 
+})
+
+
+
+
+it("failure login", ()=>{
+
+   cy.fixture('user_data.json').then((userData) =>{
+      userData.users.forEach((user)=>{
+       if(user.username == "locked_out_user"){
+       
+	 cy.LogIn(user.username, user.password)	
+         cy.get("#login-button").click();
+
+         cy.Check(".error-button",['[data-test="error"]',"Epic sadface: Sorry, this user has been locked out."])
+         }
+      
+      })
+    }) 
+})
+
+
+
+it("incorrect inputs",()=>{
+   cy.fixture("incorrect_input.json").then((wrongInput) =>{
+      wrongInput.wrong_input.forEach((input) =>{
+	//if username is empty get username required message
+        if(input.username === "empty"){
+	       
+		 cy.get('input[placeholder="Username"]').type("{enter}")
+
+
+		 cy.Check(".error-button",['[data-test="error"]',"Epic sadface: Username is required"])
+		 cy.CleanUp(".error-button",['input[placeholder="Username"]','input[placeholder="Password"]'])
+
+		 //get message after typing password without typing username 
+
+		 cy.get('input[placeholder="Password"]').type(input.password).type("{enter}");
+		 cy.Check(".error-button",['[data-test="error"]',"Epic sadface: Username is required"])
+		 cy.CleanUp(".error-button",['input[placeholder="Username"]','input[placeholder="Password"]'])
+
+	}
+        //if password is empty get password is required message
+	else if(input.password === "empty"){
+         	       
+		 cy.get('input[placeholder="Username"]').type(input.username)
+		 cy.get('input[placeholder="Password"]').type("{enter}")
+
+		 cy.Check(".error-button",['[data-test="error"]',"Epic sadface: Password is required"])
+
+		 cy.CleanUp(".error-button",['input[placeholder="Username"]','input[placeholder="Password"]'])
+
+	}
+
+        else
+	{
+
+
+		 cy.LogIn(input.username, input.password)	
+		 
+		 cy.get("#login-button").click();
+		 
+		 cy.Check(".error-button",['[data-test="error"]',"Epic sadface: Username and password do not match any user in this service"])
+		
+		 cy.CleanUp(".error-button",['input[placeholder="Username"]','input[placeholder="Password"]'])
+        	}
+          })
+	})
+           
+   
+   
+   })
+
+
+})
+
+
---
+++ cypress/support/commands.ts
@@ -0,0 +1,67 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+/// <reference types="@cypress/xpath"/>
+
+
+Cypress.Commands.add("CleanUp", (selectorToclick: string, selectorToclean: any[]) =>  {
+
+   cy.get(selectorToclick).click()
+   cy.get(selectorToclick).should("not.exist")
+    
+   selectorToclean.forEach((item)=>{  
+            
+     cy.get(item).clear()
+   
+   })
+  
+
+})
+
+
+
+Cypress.Commands.add("Check", (selectorBeenabled: string, selectorText: any[]) =>  {
+
+   cy.get(selectorBeenabled).should('be.enabled')
+    
+   cy.get(selectorText[0]).should("have.text", selectorText[1]) 
+
+})
+
+Cypress.Commands.add("LogIn",(username:string,password:string)=>{
+         
+
+         cy.get('input[placeholder="Username"]').type(username)
+         cy.get("#user-name").should('have.value', username);
+         cy.get('input[placeholder="Password"]').type(password);
+         cy.get("#password").should('have.value', password);
+
+
+
+})
+
+
